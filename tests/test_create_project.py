8021ef06a9bb1c41501754ba9896b9439e03ea5e
Mirzomumin <mirzomumin@list.ru>
2025-03-07 04:08:36 +0500

    Add external service mocker to test

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -1,31 +1,42 @@
 import pytest
+from unittest.mock import patch, AsyncMock
 
 @pytest.mark.asyncio(loop_scope="session")
 async def test_create_project_success(client):
+    mock_tasks = [
+        {"name": "Task 1", "status": "pending"},
+        {"name": "Task 2", "status": "pending"},
+    ]
     data = {
         "project_name": "Restaurant",
         "location": "San Francisco"
     }
-    response = await client.post(
-        "/projects/",
-        json=data,
-    )
 
-    response_data = response.json()
+    with patch(
+        "app.services.GeminiService.generate_content",
+        new_callable=AsyncMock,
+        return_value=mock_tasks,
+    ):
+        response = await client.post(
+            "/projects/",
+            json=data,
+        )
 
-    ######### assert response ##########
-    assert response.status_code == 200
-    assert response_data['project_name'] == data['project_name']
-    assert response_data['location'] == data['location']
-    assert response_data['status'] == 'processing'
-    assert isinstance(response_data['tasks'], list)
-    assert 'id' in response_data
+        response_data = response.json()
 
-    if response_data['tasks']:
-        task0 = response_data['tasks'][0]
-        assert 'name' in task0
-        assert 'status' in task0
-    ####################################
+        ######### assert response ##########
+        assert response.status_code == 200
+        assert response_data['project_name'] == data['project_name']
+        assert response_data['location'] == data['location']
+        assert response_data['status'] == 'processing'
+        assert isinstance(response_data['tasks'], list)
+        assert 'id' in response_data
+
+        if response_data['tasks']:
+            task0 = response_data['tasks'][0]
+            assert 'name' in task0
+            assert 'status' in task0
+        ####################################
 
 
 @pytest.mark.asyncio(loop_scope="session")
